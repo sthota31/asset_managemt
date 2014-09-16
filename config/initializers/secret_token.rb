fc9233b7ff7a882526b4da9eb04b33eab22f76bf
sthota <sthota@evoketechnologies.com>
2014-09-16 19:55:25 +0530

    initialized application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetManagement::Application.config.secret_token = 'ab35ea027862eaa53308806187b949e6ec5335c7e6b9973bc97928aedf542bfa8ddfd61fe157845dbc45dc2a5eea834395b601007bc490cb24d43ecb072895d0'
